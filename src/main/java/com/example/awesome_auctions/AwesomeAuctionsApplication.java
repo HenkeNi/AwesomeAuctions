b5245e49d23e63a35175bfe895b0a3f5cc6e24b8
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-20 13:25:00 +0100

    java spring initialized

---
+++ src/main/java/com/example/awesome_auctions/AwesomeAuctionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.awesome_auctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeAuctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeAuctionsApplication.class, args);
+	}
+
+}
