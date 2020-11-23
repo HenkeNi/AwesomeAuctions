d6d362764ddda603c51dbdb7843ac96b3ca390d5
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-23 11:44:38 +0100

    bid service

--- src/main/java/com/example/awesome_auctions/services/BidService.java
+++ src/main/java/com/example/awesome_auctions/services/BidService.java
@@ -1,4 +1,67 @@
 package com.example.awesome_auctions.services;
 
+import com.example.awesome_auctions.entities.Auction;
+import com.example.awesome_auctions.entities.Bid;
+import com.example.awesome_auctions.repositories.AuctionRepo;
+import com.example.awesome_auctions.repositories.BidRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
 public class BidService {
+
+
+    @Autowired
+    private BidRepo bidRepo;
+
+
+
+    public List<Bid> findAll() {
+        return bidRepo.findAll();
+    }
+
+
+
+    public Bid findById(String id) {
+        return bidRepo.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Bid not found by id"));
+    }
+
+   /* public List<Bid> findByUserID(String id) {
+        return bidRepo.findByUserID(id);
+    }
+
+    public List<Bid> findByAuctionID(String id) {
+        return bidRepo.findByAuctionID(id);
+    }
+*/
+
+
+    public Bid save(Bid bid) {
+        return bidRepo.save(bid);
+    }
+
+
+
+    public void update(String id, Bid bid) {
+        if (!bidRepo.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Bid not found");
+        }
+        bid.setId(id);
+        bidRepo.save(bid);
+    }
+
+
+    public void delete(String id) {
+        if (!bidRepo.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Bid not found");
+        }
+        bidRepo.deleteById(id);
+    }
+
+
 }
