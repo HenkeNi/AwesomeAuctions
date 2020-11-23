ede9a6df618c01f5bf44f825c1363e8e5b02950f
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-23 11:43:41 +0100

    Repository for user

--- src/main/java/com/example/awesome_auctions/repositories/UserRepo.java
+++ src/main/java/com/example/awesome_auctions/repositories/UserRepo.java
@@ -1,4 +1,16 @@
 package com.example.awesome_auctions.repositories;
 
-public interface UserRepo {
+import com.example.awesome_auctions.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepo extends MongoRepository<User, String> {
+
+    List<User> findByName(String name);
+    List<User> findByNameContains(String name);
+
+
 }
