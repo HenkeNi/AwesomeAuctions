021035b95458101bd5e3a61d13243f01bce63a52
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-23 11:42:46 +0100

    Fixed Bid entity

--- src/main/java/com/example/awesome_auctions/entities/Bid.java
+++ src/main/java/com/example/awesome_auctions/entities/Bid.java
@@ -1,4 +1,24 @@
 package com.example.awesome_auctions.entities;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+
+@Data
+@NoArgsConstructor
 public class Bid {
+
+    @Id
+    private String id;
+    private String userID;
+    private String auctionID;
+    private double price;
+
+    public Bid(String userID, String auctionID, double price) {
+        this.userID = userID;
+        this.auctionID = auctionID;
+        this.price = price;
+    }
 }
