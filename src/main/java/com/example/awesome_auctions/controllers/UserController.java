25205ede4938243d79729d90de9e9ca428c5d8e4
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-23 11:44:24 +0100

    controller for user

--- src/main/java/com/example/awesome_auctions/controllers/UserController.java
+++ src/main/java/com/example/awesome_auctions/controllers/UserController.java
@@ -1,4 +1,57 @@
 package com.example.awesome_auctions.controllers;
 
+
+import com.example.awesome_auctions.entities.User;
+import com.example.awesome_auctions.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<List<User>> findUser(@RequestParam(required = false) String name) {
+        var user = userService.findAll();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        var savedUser = userService.save(user);
+        return ResponseEntity.created(URI.create("/api/v1/user/" + savedUser.getId())).body(savedUser);
+    }
+
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateUser(@PathVariable String id, @RequestBody User user) {
+        userService.update(id, user);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable String id) {
+        userService.delete(id);
+    }
+
+
+
 }
