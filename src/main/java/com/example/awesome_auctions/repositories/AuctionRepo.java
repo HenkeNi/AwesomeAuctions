3202086bad3f6b6bb791a0c220e754b88d2ab378
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-23 11:43:28 +0100

    Repository for auction

--- src/main/java/com/example/awesome_auctions/repositories/AuctionRepo.java
+++ src/main/java/com/example/awesome_auctions/repositories/AuctionRepo.java
@@ -1,4 +1,18 @@
 package com.example.awesome_auctions.repositories;
 
-public interface AuctionRepo {
+import com.example.awesome_auctions.entities.Auction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AuctionRepo extends MongoRepository<Auction, String> {
+
+    List<Auction> findByProductNameContains(String productName);
+    List<Auction> findByProductName(String productName);
+
+    //Optional<Auction> findById(String id);
+
 }
