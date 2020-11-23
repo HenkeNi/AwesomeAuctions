9896f140d048c4912eb3d57a34dea1ff90f4361e
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-23 11:43:07 +0100

    An entity for Auction

--- src/main/java/com/example/awesome_auctions/entities/Auction.java
+++ src/main/java/com/example/awesome_auctions/entities/Auction.java
@@ -1,4 +1,29 @@
 package com.example.awesome_auctions.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+
+
+@Data
+@NoArgsConstructor
 public class Auction {
+
+    @Id
+    private String id;
+    private String productName;
+    private String description;
+    private String imageURL;
+    private double startBid;
+    private long endDate;
+
+    public Auction(String productName, String description, String imageURL, double startBid, long endDate) {
+        this.productName = productName;
+        this.description = description;
+        this.imageURL = imageURL;
+        this.startBid = startBid;
+        this.endDate = endDate;
+    }
 }
