169f9bda3b20def82cbf43873789fa48db928c43
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-23 11:42:56 +0100

    Fixed entity for user

--- src/main/java/com/example/awesome_auctions/entities/User.java
+++ src/main/java/com/example/awesome_auctions/entities/User.java
@@ -1,4 +1,26 @@
 package com.example.awesome_auctions.entities;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+
+@Data
+@NoArgsConstructor
 public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+    private String password;
+
+    public User(String name, String phone, String email, String password) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
 }
