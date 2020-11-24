d1de4a49d5c38cf4b6d698f1ac56ad56bcdb56ef
Hassan <pkhajeh@gmail.com>
2020-11-24 12:11:33 +0100

    started to create header

---
+++ src/main/resources/static/src/components/Header.vue
@@ -0,0 +1,22 @@
+
+<template>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo center">Logga in/ Skapa konto</a>
+      <ul class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li class="active"><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+  
+ <script>
+import M from 'materialize-css'
+export default {
+    mounted () {
+    M.AutoInit()
+},
+}
+</script>
