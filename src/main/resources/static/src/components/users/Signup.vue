84ecc8fc683a724e6fc4904588d28d36386f8386
Hampus Nilsson <hampus10699@gmail.com>
2020-11-26 14:31:26 +0100

    worked on the singup method for registration.

---
+++ src/main/resources/static/src/components/users/Signup.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col s12 m10 offset-m1">
+        <div class="grey lighten-4 black-text center">
+          <h4>Skapa konto</h4>
+          <hr class="hr-style" />
+          <form @submit.prevent="signup">
+            <div class="input-field">
+              <input type="text" id="text" v-model="name" />
+              <label for="email">Namn</label>
+            </div>
+            <div class="input-field">
+              <input type="email" id="email" v-model="email" />
+              <label for="email">E-post</label>
+            </div>
+            <div class="input-field">
+                <input type="number" id="phone-number" v-model="phoneNumber" />
+                <label for="phone-number">Telefonnummer:</label>
+            </div>
+            <div class="input-field">
+              <input type="password" id="password" v-model="password" />
+              <label for="password">Lösenord</label>
+            </div>
+            <div class="alert center red-text">
+              
+            </div>
+            <button type="submit" class="btn btn-small waves-effect">Skapa konto</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//import axios from 'axios' 
+
+export default {
+    name: 'register',
+    data: function() {
+        return {
+            name: '',
+            phoneNumber: '',
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async signup(){
+            fetch('http://localhost:5000/api/v1/user/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: this.name,
+                    phone: this.phoneNumber,
+                    email: this.email,
+                    password: this.password
+                })
+            }).then(res => {
+                return res.json()
+            }).then(data => console.log(data))
+        }
+    }
+}
+</script>
+<style lang="css" scoped>
+button {
+  margin-top: 5%;
+}
+.hr-style {
+  border: 0;
+  height: 1px;
+  margin: 0 0 10px 0;
+  background: #fff;
+  background-image: -webkit-linear-gradient(
+    left,
+    rgb(255, 255, 255),
+    rgba(184, 10, 103, 0.993),
+    rgb(255, 255, 255)
+  );
+}
+.btn {
+  background: rgb(230, 12, 128);
+}
+</style>
