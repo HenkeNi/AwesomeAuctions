0d69b37955adad29fe12baecc34ee33fc374179f
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-26 14:46:27 +0100

    go to auction details on click

--- src/main/resources/static/src/components/AuctionListItem.vue
+++ src/main/resources/static/src/components/AuctionListItem.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="auction-container">
-    <h3>{{ auction.productName }}</h3>
-    <h4>Starting bid: {{ auction.startBid }}</h4>
-    <h4>Ends at: {{ auction.endDate }}</h4>
-  </div>
+  <div @click="goToAuctionDetails(auction)" class="auction-item">
+    <img class="item-img" src="https://i.ebayimg.com/images/g/N0oAAOSwmo1fdoDf/s-l640.jpg" />
+    <div class="item-desc">
+      <h2>{{ auction.productName }}</h2>
+      <h4>Starting bid: {{ auction.startBid }}</h4>
+      <h4>Ends at: {{ auction.endDate }}</h4>
+    </div>
+   </div>
 </template>
 
 <script>
@@ -16,11 +19,41 @@ export default {
     return {
 
     };
+  },
+  methods: {
+    goToAuctionDetails(auction) {
+      console.log("Passing auction: ", auction);
+      this.$router.push(`auctionDetail/${auction}`);
+    }
   }
 }
 </script>
 
 <style>
+.auction-item {
+  width: 25vw;
+  margin: 5px 20px 5px 20px;
+  padding-left: 15px;
+  cursor: pointer;
+}
 
+.item-desc {
+  text-align: start;
+  padding-left: 25px;
+}
 
+.item-desc h2 {
+  margin-top: 2px;
+}
+
+.auction-item img {
+  flex-shrink: 0;
+  max-width: 60%;
+  max-height: 60%;
+}
+
+/* .item-img {
+  width: 20vw;
+  height: 30vw;
+} */
 </style>
