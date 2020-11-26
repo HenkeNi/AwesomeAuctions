6e6cb1304dc9cd89b950402f18b55d9f450a5d33
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-26 11:53:38 +0100

    Changed data to computed property

---
+++ src/main/resources/static/src/components/AuctionList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h3>Current Auctions:</h3>
+    <div class="article-list">
+      <AuctionListItem
+      v-for="auction in auctionsData"
+      :key = "auction.id"
+      :auction="auction"
+     />
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import AuctionListItem from './AuctionListItem';
+
+export default {
+
+  components: {
+    AuctionListItem
+  },
+   data() {
+    return {
+      //auctions: this.$store.getters.auctionList,
+    };
+  },
+
+  computed: {
+    auctionsData() {
+      return this.$store.getters.auctionList;
+    }
+  },
+
+
+ 
+
+}
+</script>
+
+<style scoped>
+  .article-list {
+    background-color: lightblue;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    
+  }
+</style>
