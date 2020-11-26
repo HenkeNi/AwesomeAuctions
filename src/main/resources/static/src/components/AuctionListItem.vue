dcf80cd6dfc8c5ba63564984b5be2ad38360cfde
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-26 11:53:59 +0100

    passing prop to aucitonListItem

---
+++ src/main/resources/static/src/components/AuctionListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="auction-container">
+    <h3>{{ auction.productName }}</h3>
+    <h4>Starting bid: {{ auction.startBid }}</h4>
+    <h4>Ends at: {{ auction.endDate }}</h4>
+  </div>
+</template>
+
+<script>
+
+// import prop...
+
+export default {
+  props: ['auction'],
+  data() {
+    return {
+
+    };
+  }
+}
+</script>
+
+<style>
+
+
+</style>
