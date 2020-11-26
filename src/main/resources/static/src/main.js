1e64ad6a436007079550dd546959a253ddac4e7b
Hampus Nilsson <hampus10699@gmail.com>
2020-11-26 14:31:09 +0100

    yes

--- src/main/resources/static/src/main.js
+++ src/main/resources/static/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'materialize-css/dist/css/materialize.min.css'
+import 'material-design-icons/iconfont/material-icons.css'
+import store from './store'
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
