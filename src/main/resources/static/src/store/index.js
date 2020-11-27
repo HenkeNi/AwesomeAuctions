c9627919aa89e1e93adddf68d29550f881537105
Henrik Jangefelt <henke9105@hotmail.com>
2020-11-27 12:39:53 +0100

    saving searched auction i store

--- src/main/resources/static/src/store/index.js
+++ src/main/resources/static/src/store/index.js
@@ -6,26 +6,28 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     auctionList: [],
+    auctionSearched: "",
   },
   getters: {
     auctionList(state) {
       return state.auctionList;
-    }
+    },
+    auctionSearched(state) {
+      return state.auctionSearched;
+    },
   },
   mutations: {
     setAuctionList(state, auctionList) {
-      console.log("SETTING AUCTIONS", auctionList);
       state.auctionList = auctionList;
-
-      console.log(state.auctionList);
+    },
+    setAuctionSearched(state, searched) {
+      state.auctionSearched = searched;
     }
   },
   actions: {
     async fetchAuctions({ commit }) {
       let res = await fetch('http://localhost:5000/api/v1/auction');
       let json = await res.json();
-
-      console.log("FETCHING AUCTIONS", json);
 
       commit('setAuctionList', json);
     },
