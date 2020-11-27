55be309729f5d506a9c06fff5ffc699f7c6bc3b2
Victor <victortornbom@gmail.com>
2020-11-27 14:05:35 +0100

    added buttons, text, inputfields

---
+++ src/main/resources/static/src/pages/CreateAuction.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="container">
+  <div class="auction">
+      <h1>Create new auction</h1>
+      <p class="test">Product name</p>
+      <input class="product-name">
+      <br>
+      <br>
+      <p class="test">Asking bid</p>
+      <input class="asking-bid" type="number">
+      <br>
+        <button class="create-auction btn waves-effect waves-light" type="submit" name="action">Create auction</button>
+  </div>
+  </div>
+</template>
+
+<script>
+
+import M from 'materialize-css'
+
+export default {
+
+
+    mounted () {
+        M.AutoInit()
+    }
+}
+
+</script>
+
+
+<style>
+.auction {
+    margin:0 auto;
+}
+
+.product-name {
+    width: 40% !important;
+}
+
+.asking-bid {
+    width: 40% !important;
+}
+
+.test {
+    margin-bottom: 0;
+}
+
+.create-auction {
+    margin-top: 40px;
+}
+
+</style>
