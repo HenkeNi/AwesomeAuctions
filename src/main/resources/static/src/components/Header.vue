d238de022ebc552132f066a58cdaad1cb03f0ada
Ola Wiebe <eeken07@gmail.com>
2020-11-30 10:54:07 +0100

    fix header with materialize

--- src/main/resources/static/src/components/Header.vue
+++ src/main/resources/static/src/components/Header.vue
@@ -2,38 +2,70 @@
 <template>
   <nav>
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Awesome Auction</a>
+      <a href="#" class="brand-logo center">Awesome Auction</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="sass.html">Home</a></li>
+        <li><a href="badges.html">About</a></li>
+        <li><a class="modal-trigger" href="#login">Log In</a>
+
+  <!-- Modal Structure -->
+  <div id="login" class="modal">
+    <div class="container-fluid">
+      <div class="row login">
+        <div class="col s12 m8 offset-m2">
+          <div class="center text-darken">
+            <h4>Log in</h4>
+            <!-- <hr class="hr-style"> -->
+            <form @submit.prevent="login">
+              <div class="input-field">
+                <input type="email" id="email" v-model="email" />
+                <label for="email">E-mail</label>
+              </div>
+              <div class="input-field">
+                <input type="password" id="password" v-model="password" />
+                <label for="password">Password</label>
+              </div>
+              <button
+                type="submit"
+                class="btn btn-small waves-effect"
+              >Log in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+        <li><a href="collapsible.html">Register</a></li>
       </ul>
     </div>
   </nav>
 
-       <!-- Modal Structure -->
-  <!-- <div id="modal1" class="modal">
-    <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div> -->
 
 
 
 </template>
   
- <script>
+<script>
 import M from 'materialize-css'
-
 export default {
+
+  data() {
+    return {
+      email: "",
+      password: "",
+    
+    }
+  },
+  methods: {
+    login(){
+      console.log('u tried to log in');
+    }
+  },
+    
     mounted () {
     M.AutoInit();
-    let modal = document.querySelectorAll(".modal");
-    this.$M.Modal.init(modal);
-},
+    }
 }
 
 </script>
