f6beff474de6fd649d601c6298c62121887bdcb6
Ola Wiebe <eeken07@gmail.com>
2020-12-01 12:55:52 +0100

    add function in backend for log in with yusra

--- src/main/java/com/example/awesome_auctions/controllers/UserController.java
+++ src/main/java/com/example/awesome_auctions/controllers/UserController.java
@@ -7,10 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -20,6 +31,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Resource(name="authenticationManager")
+    private AuthenticationManager authManager;
 
     @GetMapping
     public ResponseEntity<List<User>> findUser(@RequestParam(required = false) String name) {
@@ -38,6 +51,24 @@ public class UserController {
         return ResponseEntity.created(URI.create("/api/v1/user/" + savedUser.getId())).body(savedUser);
     }
 
+    @PostMapping("/login")
+    private ResponseEntity<User> securityLogin(String email, String password, HttpServletRequest req) {
+        UsernamePasswordAuthenticationToken authReq
+                = new UsernamePasswordAuthenticationToken(email, password);
+        Authentication auth = authManager.authenticate(authReq);
+
+        if(!auth.isAuthenticated()) {
+            throw new BadCredentialsException("Wrong username or password");
+        }
+
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+        HttpSession session = req.getSession(true);
+        session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, sc);
+
+        return ResponseEntity.ok(userService.findCurrentUser());
+    }
+
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -51,7 +82,6 @@ public class UserController {
     public void deleteUser(@PathVariable String id) {
         userService.delete(id);
     }
-
 
 
 }
