fc133acbf098b7390d187fd382e77d8fd2ea5bd9
Ola Wiebe <eeken07@gmail.com>
2020-12-01 12:56:11 +0100

    create myuserdetailservice

--- src/main/java/com/example/awesome_auctions/services/MyUserDetailsService.java
+++ src/main/java/com/example/awesome_auctions/services/MyUserDetailsService.java
@@ -1,4 +1,50 @@
 package com.example.awesome_auctions.services;
 
-public class MyUserDetailsService {
+import com.example.awesome_auctions.entities.User;
+import com.example.awesome_auctions.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class MyUserDetailsService implements UserDetailsService {
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    public BCryptPasswordEncoder getEncoder() { return encoder; }
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found by name: " + email);
+        }
+        return toUserDetails(user);
+    }
+
+    public User addUser(String email, String password){
+        User user = new User(email, encoder.encode(password));
+        try {
+            return userRepo.save(user);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private UserDetails toUserDetails(User user) {
+        // If you have a User entity you have to
+        // use the userdetails User for this to work
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER").build();
+    }
 }
