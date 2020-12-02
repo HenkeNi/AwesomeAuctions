2e456c272b5bab2f0c8d15457f688eb2c887f9a5
Hassan <pkhajeh@gmail.com>
2020-12-02 08:57:52 +0100

    I fixed contact just on V i left who can fix it?

--- src/main/resources/static/src/components/Kontakt.vue
+++ src/main/resources/static/src/components/Kontakt.vue
@@ -2,17 +2,19 @@
 <template>
 
  <div class="row">
-    <div class="col s12 m6">
-      <div class=" card blue darken-1 ">
+    <div class="card cover">
+
+      <div class=" card center-align blue darken-1 ">
         <div class="card-content white-text">
           <span class="card-title">About us</span>
-          <h2>Here you can buy what you need and sell what you dont't need. You can suggest higher price also.</h2>
+          <h2>Here you can buy what you need and sell what you don't need. You can suggest higher price also.</h2>
         </div>
         <div class="card-action red-text">
-          <h2>Contact Us</h2>
+          <h2 class="yellow-text">Contact Us</h2>
           <ul>
-         <li> <a href="#">Send Mail</a></li>
-          <li><a href="#">Ring Us</a></li>
+         <li> <a class="btn-large" href="#">Send Mail</a></li>
+         <br/>
+          <li><a class="btn-large" href="#">Ring Us</a></li>
           </ul>
         </div>
       </div>
