dbae8545b0bd4c0a5891cd6cb8cc56e0a7a0d9e2
Hampus Nilsson <hampus10699@gmail.com>
2020-12-09 11:26:57 +0100

    added hover effect to links

--- src/main/resources/static/src/components/Footer.vue
+++ src/main/resources/static/src/components/Footer.vue
@@ -3,9 +3,9 @@
     <div>
       <a href="#" class="brand-logo center">Connect with us!</a>
       <ul id="nav-mobile" class="center hide-on-med-and-down">
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="badges.html">Facebook</a></li>
-        <li><a href="collapsible.html">Twitter</a></li>
+        <li><a class="hover-effect" href="/contact">Contact</a></li>
+        <li><a class="hover-effect" href="badges.html">Facebook</a></li>
+        <li><a class="hover-effect" href="collapsible.html">Twitter</a></li>
       </ul>
     </div>
   </nav>
@@ -28,7 +28,13 @@ export default {
 .footer{
   background: -webkit-linear-gradient(to top, rgb(44, 36, 156), #dfd3fb);
   background: linear-gradient(to top, rgb(255, 2, 141), rgb(117, 9, 67));
+  box-shadow:0px 0px 15px black;
+  font-weight:bold;
   position: fixed;
   bottom: 0;
+}
+
+.hover-effect:hover{
+  transform:translate(4px,3px);
 }
 </style>
