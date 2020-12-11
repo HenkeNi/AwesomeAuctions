148bad6f7d79bd6c9800e1aff054d7fc4c79b89c
Hampus Nilsson <hampus10699@gmail.com>
2020-12-11 12:46:28 +0100

    styling

--- src/main/resources/static/src/pages/Contact.vue
+++ src/main/resources/static/src/pages/Contact.vue
@@ -1,32 +1,24 @@
 
 <template>
-
- <div class="row">
-    <div class="card cover">
-
-      <div class=" card center-align blue darken-1 ">
-        <div class="card-content white-text">
+ <div class="row transparent">
+    <div class="card cover transparent">
+      <div class=" card center-align transparent">
+        <div class="card-content white-text transparent">
           <span class="card-title text">About us</span>
           <h2 class="text">Here you can buy what you need and sell what you don't need. You can suggest higher price also.</h2>
         </div>
-        <div class="card-action red-text">
-          <h2 class="yellow-text">Contact Us</h2>
+        <div class="card-action transparent">
+          <h2 class="text" >Contact Us</h2>
           <ul>
-         <li> <a class="btn-large" href="#">Send Mail</a></li>
+         <li> <a class="btn-large btn-style" href="#">Send Mail</a></li>
          <br/>
-          <li><a class="btn-large" href="#">Call Us</a></li>
+          <li><a class="btn-large btn-style" href="#">Call Us</a></li>
           </ul>
         </div>
       </div>
       </div>
      
   </div>
-  
-
-      
-
-
-
 </template>
 
  <script>
@@ -43,7 +35,15 @@ export default {
 
 <style>
 .text {
-  color: rgb(100, 98, 98);
+  color: white;
+}
+
+.btn-style{
+  background-color: rgb(188,140,78);
+}
+
+.card-action{
+  border:none;
 }
 
 </style>
