cbba3ed675fc9dcb2287cf126d4a9ac88ca2b4ae
Hampus Nilsson <hampus10699@gmail.com>
2020-12-11 12:46:58 +0100

    removed border and box shadow

--- src/main/resources/static/src/pages/HomePage.vue
+++ src/main/resources/static/src/pages/HomePage.vue
@@ -35,9 +35,12 @@ export default {
   margin: auto;
   margin-top: 15px;
   padding-top: 20px;
-  box-shadow:0 0 15px 4px rgb(0, 0, 0);
+  color:white;
   border-radius: 10px;
-  border: 1px solid red;
+}
+
+.input-field{
+  color:white
 }
 
 
